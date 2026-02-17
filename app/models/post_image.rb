4bda8433ce7712cdef95d9afe22053a22f037b1b
jigangdade60 <255278098+jigangdade60@users.noreply.github.com>
2026-02-17 20:47:43 +0000

    [Finish] 4-20

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,6 +5,9 @@ belongs_to :user
 has_many :post_comments, dependent: :destroy
 has_many :favorites, dependent: :destroy
 
+validates :shop_name, presence: true
+validates :image, presence: true
+
 
  def get_image
     unless image.attached?
